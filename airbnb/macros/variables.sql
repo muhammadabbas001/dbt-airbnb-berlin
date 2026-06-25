6d3bc314441d0d4073d93237739c5f51332958f0
Muhammad Abbas <muhammdabbas.cs@gmail.com>
2026-06-25 16:51:04 +0500

    variables done

---
+++ airbnb/macros/variables.sql
@@ -0,0 +1,9 @@
+{% macro learn_variables() %}
+
+    {% set your_name_jinja = "Hanzala" %}
+    {{ log("Hello " ~ your_name_jinja, info=True) }}
+
+    {{ log("Hello dbt user " ~ var("user_name", "NO USERNAME IS SET!!") ~ "!", info=True) }}
+
+
+{% endmacro %}
